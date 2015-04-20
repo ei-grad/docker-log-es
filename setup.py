866942bd50f97d24f0dd74931adbfaf92d092bd8
Andrew Grigorev <andrew@ei-grad.ru>
2015-04-20 13:29:05 +0300

    Add Dockerfile, requirements.txt (for python:3-onbuild), fix setup.py

--- setup.py
+++ setup.py
@@ -4,23 +4,21 @@ import os
 from setuptools import setup, find_packages
 
 
-scripts = filter(
-    lambda x: os.access(x, os.X_OK),
-    [os.path.join('bin', i) for i in os.listdir("bin") if os.path.isfile(os.path.join('bin', i))]
-)
+scripts = [x for x in [
+    os.path.join('bin', i)
+    for i in os.listdir("bin")
+    if os.path.isfile(os.path.join('bin', i))
+] if os.access(x, os.X_OK)]
 
 
 setup(
     name='docker-log-es',
     scripts=scripts,
     version='0.0.1',
-    description='Pushing Docker logs to elasticsearch',
+    description='Put Docker logs to ElasticSearch',
     author='Andrew Grigoriev, Dmitry Orlov',
     author_email='andrew@ei-grad.ru, me@mosquito.su',
     url='https://github.com/ei-grad/docker-log-es',
     packages=find_packages(),
-    install_requires=(
-        'tornado',
-        'ujson',
-    )
+    install_requires=['tornado', 'elasticsearch', 'ujson'],
 )
